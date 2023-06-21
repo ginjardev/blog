56ad581c0af8d910ea42423edb92ab262116a177
ginjardev <arthurshur@gmail.com>
2023-06-21 22:05:43 +0000

    email and update form

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -13,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Publish'
 
     title = models.CharField(max_length=250)
-    slug  = models.SlugField(max_length=250)
+    slug  = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -31,4 +34,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
+
 
