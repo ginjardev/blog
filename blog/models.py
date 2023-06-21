0722ad30bd94869338a5b3698b5a6e83fccebdae
ginjardev <arthurshur@gmail.com>
2023-06-21 11:18:06 +0000

    blog templates

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status = Post.Status.PUBLISHED)
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -15,6 +20,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
