c9f11ded84e529836771cdf40a5d4d0e8f31a4e3
ginjardev <arthurshur@gmail.com>
2023-06-22 22:18:20 +0000

    comment for blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -27,6 +29,24 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     
     return render(request, 'blog/post/detail.html', {'post':post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{clean_data['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{clean_data['name']}\'s comments: {clean_data['comments']}"
+            send_mail(subject, message, 'arthurshur@gmail.com',[clean_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent': sent})
+
 
 
 class PostListView(ListView):
