a9e523b7713080fc31f61d604a48aa6a8f69fecd
ginjardev <arthurshur@gmail.com>
2023-06-22 23:52:48 +0000

    templates

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -27,8 +28,9 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    
-    return render(request, 'blog/post/detail.html', {'post':post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post':post, 'comments': comments, 'form': form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
@@ -48,6 +50,18 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent': sent})
 
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,id= post_id,  status= Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})    
 
 class PostListView(ListView):
     queryset = Post.published.all()
