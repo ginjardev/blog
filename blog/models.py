35060c3c4bd6526b259f6c5dc52025206c5283ee
ginjardev <arthurshur@gmail.com>
2023-06-25 22:10:01 +0000

    tag feature

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager()
 
 
 
@@ -25,6 +26,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
