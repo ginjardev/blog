b09995ee7a60d792719d946836ca05e9fde1b86e
ginjardev <arthurshur@gmail.com>
2023-06-25 22:10:55 +0000

    tag feature

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from taggit.managers import TaggableManager()
+from taggit.managers import TaggableManager
 
 
 
