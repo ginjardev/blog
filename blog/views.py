4cbfd13751017101ee841db17f1cabcf62cc3614
ginjardev <arthurshur@gmail.com>
2023-07-01 01:35:49 +0000

    custom tag functionality

--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,14 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
     tag = None
     if tag_slug:
-        tag = get_object_or_404(Tag, tag = tag_slug)
+        tag = get_object_or_404(Tag, slug=tag_slug)
         post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -35,7 +36,11 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post':post, 'comments': comments, 'form': form})
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in = post_tag_ids).exclude(id = post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', 
+                  {'post':post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = Post.Status.PUBLISHED)
