9421e3d3a79bbb368fde52bbfdfd32d45fde5b8b
ginjardev <arthurshur@gmail.com>
2023-07-03 23:41:01 +0000

    link to postgresql

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = "blogapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'qqqqqq',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
