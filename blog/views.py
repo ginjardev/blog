eceef19c240a922fee3506c9eff513053d254830
ginjardev <arthurshur@gmail.com>
2023-07-06 01:39:43 +0000

    search similarity

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -84,11 +84,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
+            search_vector = SearchVector('title', 'body', weight = 'A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query, config='spanish')
             results = Post.published.annotate(
-                search = search_vector, rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
